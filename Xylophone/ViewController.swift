030add251f44a8ec266a11abbf71c805567d5ca9
“2laa-Fares” <alaafmohamed95@gmail.com>
2020-10-27 13:31:03 +0200

    Linking multiple buttons to same IBAction then handle action will done depend on which button is pressed and add delay.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -17,12 +17,19 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        playSound()
+        playSound(soundName: sender.currentTitle!)
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
     }
         
-    func playSound() {
+    func playSound(soundName: String) {
         // Get url of sound.
-        let url = Bundle.main.url(forResource: "Sounds/C", withExtension: "wav")
+        let url = Bundle.main.url(forResource: "Sounds/\(soundName)", withExtension: "wav")
         // Set the audio session category,to make audio play even if phone in silent mode.
         try! AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
         // Set file path to audio player.
